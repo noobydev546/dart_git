342876de2e652fb767bb55388a7f3e6ecb619dfe
Pattaphon-sr <zenotadeo@gmail.com>
2025-08-21 16:18:59 +0700

    initial commit

---
+++ bin/dart_git.dart
@@ -0,0 +1,12 @@
+void sum(int one, int two) {
+  // compute sum and print result
+}
+
+void difference(int one, int two) {
+  // compute difference and print result
+}
+
+void main() {
+  sum(1, 2);
+  difference(2, 1);
+}
---
+++ test/dart_git_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_git/dart_git.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
